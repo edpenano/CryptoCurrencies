a560ba9686ab87df4850c94f90275d451de3726a
Ed Penano <hi@edpenano.com>
2020-12-05 00:04:27 -0800

    adding assets

---
+++ CryptoCurrencies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCurrencies
+//
+//  Created by Ed Penano on 12/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoCurrencies/CryptoCurrenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrenciesApp.swift
+//  CryptoCurrencies
+//
+//  Created by Ed Penano on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
