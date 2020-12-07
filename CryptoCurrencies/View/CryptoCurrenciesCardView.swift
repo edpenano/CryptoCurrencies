e7a7c692db7442cac73bb552b36839d828751cdd
Ed Penano <hi@edpenano.com>
2020-12-07 10:24:51 -0800

    VStack & ZStack

--- CryptoCurrencies/View/CryptoCurrenciesCardView.swift
+++ CryptoCurrencies/View/CryptoCurrenciesCardView.swift
@@ -16,18 +16,34 @@ struct CryptoCurrenciesCardView: View {
         ZStack {
             VStack(spacing: 20) {
                 // CRYPTOCURRENCY: IMAGE
-                Image("bitcoin")
+                Image("xrp")
                     .resizable()
                     .scaledToFit()
                     // add drop-shadow
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
                 
                 // CRYPTOCURRENCY: TITLE
-                Text("Bitcoin")
+                Text("XRP")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0 , green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
                 // CRYPTOCURRENCY: HEADLINE
+                Text("XRP is a digital asset built for payments and settles transactions in 3-5 seconds.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
                 // CRYPTOCURRENCY: BUTTON
+                
+                
             } //: VSTACK
         } //: ZSTACK
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
---
+++ CryptoCurrencies/View/StartButtonView.swift
@@ -0,0 +1,26 @@
+//
+//  StartButtonView.swift
+//  CryptoCurrencies
+//
+//  Created by Karen Lee on 12/7/20.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
