55d8341d97fb84f96a60ed2df6a6c08d0ceb45a5
Ed Penano <hi@edpenano.com>
2020-12-09 00:22:21 -0800

    tabviews

---
+++ CryptoCurrencies/App/OnboardingView.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingView.swift
+//  CryptoCurrencies
+//
+//  Created by Karen Lee on 12/9/20.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                CryptoCurrenciesCardView()
+            }
+        }  //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
--- CryptoCurrencies/View/CryptoCurrenciesCardView.swift
+++ CryptoCurrencies/View/CryptoCurrenciesCardView.swift
@@ -23,6 +23,7 @@ struct CryptoCurrenciesCardView: View {
                     .scaledToFit()
                     // add drop-shadow
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.8)
                 
                 // CRYPTOCURRENCY: TITLE
                 Text("XRP")
@@ -44,9 +45,18 @@ struct CryptoCurrenciesCardView: View {
                 
             } //: VSTACK
         } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+            // resets the animation
+            isAnimating = false
+        }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: .center)
         .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
+        // Adds padding to the entire card
+        .padding(.horizontal, 20)
     }
 }
 
